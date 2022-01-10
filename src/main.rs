efee4d7e8e21f61dcdbdefd4a356142e3f1ca9ab
dylanVerstraete <dylan_verstraete1@hotmail.com>
2022-01-10 21:19:52 +0100

    feat: use subcommands properly

--- src/main.rs
+++ src/main.rs
@@ -22,51 +22,59 @@ async fn main() {
                 .help("substrate websocket connection"),
         )
         .subcommand(
-            App::new("farms").about("Query the farms.").arg(
-                Arg::new("id")
-                    .help("get by id")
-                    .takes_value(true)
-                    .required(true),
+            App::new("farms").about("Farm operations").subcommand(
+                App::new("get").about("Get farm").arg(
+                    Arg::new("farm_id")
+                        .help("get by id")
+                        .takes_value(true)
+                        .required(true),
+                ),
             ),
         )
         .subcommand(
-            App::new("balance")
-                .about("Get the balance of an account")
-                .arg(
+            App::new("balance").about("Balance operations").subcommand(
+                App::new("get").about("Get balance").arg(
                     Arg::new("account")
                         .help("the account for which to query the balance")
                         .takes_value(true)
                         .required(true),
                 ),
+            ),
         )
         .subcommand(
             App::new("node")
                 .about("Get a node registered on chain")
-                .arg(
-                    Arg::new("node_id")
-                        .help("the id of the node to fetch")
-                        .takes_value(true)
-                        .required(true),
+                .subcommand(
+                    App::new("get").about("Get Node by ID").arg(
+                        Arg::new("node_id")
+                            .help("the id of the node to fetch")
+                            .takes_value(true)
+                            .required(true),
+                    ),
                 ),
         )
         .subcommand(
             App::new("contract")
                 .about("Get a contract registered on chain")
-                .arg(
-                    Arg::new("contract_id")
-                        .help("the id of the contract to fetch")
-                        .takes_value(true)
-                        .required(true),
+                .subcommand(
+                    App::new("get").about("Get Contract by ID").arg(
+                        Arg::new("contract_id")
+                            .help("the id of the contract to fetch")
+                            .takes_value(true)
+                            .required(true),
+                    ),
                 ),
         )
         .subcommand(
             App::new("twin")
                 .about("Get a twin registered on chain")
-                .arg(
-                    Arg::new("twin_id")
-                        .help("the id of the twin to fetch")
-                        .takes_value(true)
-                        .required(true),
+                .subcommand(
+                    App::new("get").about("Get Twin by ID").arg(
+                        Arg::new("twin_id")
+                            .help("the id of the twin to fetch")
+                            .takes_value(true)
+                            .required(true),
+                    ),
                 ),
         )
         .get_matches();
@@ -75,13 +83,12 @@ async fn main() {
 
     match matches.subcommand() {
         Some(("farms", farm_matches)) => {
-            if farm_matches.is_present("id") {
-                let from = AccountKeyring::Alice.pair();
-                let client = tfchain_client::Client::new(String::from(websocket), from);
-                let id = farm_matches.value_of("id").unwrap();
-                match id.parse::<u32>() {
-                    Ok(x) => {
-                        let farm = client.get_farm_by_id(x).unwrap();
+            if let Some(get_farm) = farm_matches.subcommand_matches("get") {
+                match get_farm.value_of_t("farm_id") {
+                    Ok(farm_id) => {
+                        let from = AccountKeyring::Alice.pair();
+                        let client = tfchain_client::Client::new(String::from(websocket), from);
+                        let farm = client.get_farm_by_id(farm_id).unwrap();
                         println!("farm: {:?}", farm);
                     }
                     Err(e) => {
@@ -91,50 +98,58 @@ async fn main() {
             }
         }
         Some(("balance", account)) => {
-            let account = account.value_of("account").unwrap();
-            let from = AccountKeyring::Alice.pair();
-            let client = tfchain_client::Client::new(String::from(websocket), from);
-            match account.parse::<AccountId32>() {
-                Ok(ref account) => {
-                    let balance = client.get_account_free_balance(account).unwrap();
-                    println!("Free balance for account {}: {} TFT", account, balance);
-                }
-                Err(e) => {
-                    println!("{} is not a valid account ({})", account, e);
+            if let Some(get_balance) = account.subcommand_matches("get") {
+                let account = get_balance.value_of("account").unwrap();
+                let from = AccountKeyring::Alice.pair();
+                let client = tfchain_client::Client::new(String::from(websocket), from);
+                match account.parse::<AccountId32>() {
+                    Ok(ref account) => {
+                        let balance = client.get_account_free_balance(account).unwrap();
+                        println!("Free balance for account {}: {} TFT", account, balance);
+                    }
+                    Err(e) => {
+                        println!("{} is not a valid account ({})", account, e);
+                    }
                 }
             }
         }
         Some(("node", node_data)) => {
-            let from = AccountKeyring::Alice.pair();
-            let client = tfchain_client::Client::new(String::from(websocket), from);
-            match node_data.value_of_t("node_id") {
-                Ok(node_id) => {
-                    let node = client.get_node_by_id(node_id).unwrap();
-                    println!("{}", node);
+            if let Some(get_node) = node_data.subcommand_matches("get") {
+                let from = AccountKeyring::Alice.pair();
+                let client = tfchain_client::Client::new(String::from(websocket), from);
+                match get_node.value_of_t("node_id") {
+                    Ok(node_id) => {
+                        let node = client.get_node_by_id(node_id).unwrap();
+                        println!("{}", node);
+                    }
+                    Err(e) => println!("could not parse node_id: {}", e),
                 }
-                Err(e) => println!("could not parse node_id: {}", e),
             }
         }
         Some(("contract", contract_data)) => {
-            let from = AccountKeyring::Alice.pair();
-            let client = tfchain_client::Client::new(String::from(websocket), from);
-            match contract_data.value_of_t("contract_id") {
-                Ok(contract_id) => {
-                    let contract = client.get_contract_by_id(contract_id).unwrap();
-                    println!("{}", contract);
+            if let Some(get_contract) = contract_data.subcommand_matches("get") {
+                let from = AccountKeyring::Alice.pair();
+                let client = tfchain_client::Client::new(String::from(websocket), from);
+                match get_contract.value_of_t("contract_id") {
+                    Ok(contract_id) => {
+                        let contract = client.get_contract_by_id(contract_id).unwrap();
+                        println!("{}", contract);
+                    }
+                    Err(e) => println!("could not parse contract_id: {}", e),
                 }
-                Err(e) => println!("could not parse contract_id: {}", e),
             }
         }
         Some(("twin", twin_data)) => {
-            let from = AccountKeyring::Alice.pair();
-            let client = tfchain_client::Client::new(String::from(websocket), from);
-            match twin_data.value_of_t("twin_id") {
-                Ok(twin_id) => {
-                    let twin = client.get_twin_by_id(twin_id).unwrap();
-                    println!("{}", twin);
+            if let Some(get_twin) = twin_data.subcommand_matches("get") {
+                let from = AccountKeyring::Alice.pair();
+                let client = tfchain_client::Client::new(String::from(websocket), from);
+                match get_twin.value_of_t("twin_id") {
+                    Ok(twin_id) => {
+                        let twin = client.get_twin_by_id(twin_id).unwrap();
+                        println!("{}", twin);
+                    }
+                    Err(e) => println!("could not parse twin_id: {}", e),
                 }
-                Err(e) => println!("could not parse twin_id: {}", e),
             }
         }
         _ => unreachable!(), // If all subcommands are defined above, anything else is unreachable
